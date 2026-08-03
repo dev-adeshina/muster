7861cc0327216cc211710620754896384ca6d463
dev-adeshina <shina.oladoja@outlook.com>
2026-08-03 18:23:29 +0100

    welcome page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<html>
+
+<head>
+    <title>About</title>
+</head> 
+<body>
+    <h1>About Muster</h1>
+    <p>Muster is under construction.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+// Route::get('/about', 'about')->name('about');
